8dba46360a3929c06d2e5d24dba6a6ea33d234e1
0richalcos <xox.zhe@foxmail.com>
2021-06-17 17:30:51 +0800

    v1.3

---
+++ redis_session_manager/src/main/java/com/orichalcos/RedisSessionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.orichalcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSessionManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSessionManagerApplication.class, args);
+    }
+
+}
---
+++ redis_session_manager/src/test/java/com/orichalcos/config/RedisSessionManager.java
@@ -0,0 +1,9 @@
+package com.orichalcos.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisSessionManager {
+}
